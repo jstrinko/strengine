92fd7fcc7d6c3bb51277cee02d43add4e0520bd5
Jeff Strinko <jstrinko@gmail.com>
2015-02-16 04:14:53 +0000

    initial

---
+++ web/script/strengine.js
@@ -0,0 +1,13 @@
+var Strengine = function(options) {
+	_.extend(this, options);
+	fast_bindall(this);
+};
+
+_.extend(Strengine.prototype, {
+	load: function(game) {
+
+	},
+	start: function() {
+
+	}
+});
